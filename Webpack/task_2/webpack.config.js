f6493e9ccfc1dfbef88c918d4f22374afbfffac0
freddyrosa16 <freddy.rosa16@gmail.com>
2024-12-08 23:10:50 -0400

    Add Webpack configuration for dashboard with production mode and asset handling

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/dashboard_main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      /* {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          // 'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      }, */
+    ],
+  },
+};
